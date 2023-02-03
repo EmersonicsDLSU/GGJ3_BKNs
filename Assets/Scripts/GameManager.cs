02bccdd75923bfb1b476b1084e6c2159cf693595
Aldrey Gaurana <drei_gaurana@dlsu.edu.ph>
2023-02-03 23:08:13 +0800

    foundations stuff

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : tSingleton<GameManager>
+{
+    private int playerScore = 0;
+    private Dictionary<ECollectible, int> collectibleUpgradeLevel = new Dictionary<ECollectible, int>();
+    private float gameTime = 0.0f;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        
+    }
+}
---
+++ Assets/Scripts/Player-related/MPAttribs.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MPAttribs
+{
+    [Tooltip("Player current speed multiplier")]
+    public float playerSpeed = 1.0f;
+    [Tooltip("Player current health")]
+    public float playerHealth = 100.0f;
+}
