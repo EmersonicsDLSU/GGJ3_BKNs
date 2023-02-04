1f804d453abdcff0ba1b877dcae64dba6c0c9758
paolim008 <pao_lim@dlsu.edu.ph>
2023-02-04 19:19:56 +0800

    Statistics and Upgrade Panel

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class UIController : MonoBehaviour
 {
     [SerializeField] private GameObject[] gamePanels;
+
+    [SerializeField] private GameObject titleScreen;
+    [SerializeField] private GameObject titleMenu;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,12 @@ public class UIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.anyKeyDown && titleScreen.activeSelf)
+        {
+            titleScreen.SetActive(false);
+            titleMenu.SetActive(true);
+        }
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             foreach (var panel in gamePanels)
