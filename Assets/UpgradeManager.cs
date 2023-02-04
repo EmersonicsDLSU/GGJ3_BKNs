7164177b59ac0c365d56269dc03c7d426fb398f2
paolim008 <pao_lim@dlsu.edu.ph>
2023-02-04 20:47:51 +0800

    Upgrade Manager

--- Assets/UpgradeManager.cs
+++ Assets/UpgradeManager.cs
@@ -23,14 +23,13 @@ public class UpgradeManager : MonoBehaviour
 
     private void OnEnable()
     {
-        //init_Speed = gm.GetUpgradeDictionary()[ECollectible.SpeedCollectible];
-        //init_Multiplier = gm.GetUpgradeDictionary()[ECollectible.MultiplierCollectible];
-        //init_SlowDeplete = gm.GetUpgradeDictionary()[ECollectible.SlowDepleteCollectible];
+        init_Speed = gm.GetUpgradeDictionary()[ECollectible.SpeedCollectible];
+        init_Multiplier = gm.GetUpgradeDictionary()[ECollectible.MultiplierCollectible];
+        init_SlowDeplete = gm.GetUpgradeDictionary()[ECollectible.SlowDepleteCollectible];
 
-        init_Speed = 5;
-        init_Multiplier = 5;
-        init_SlowDeplete = 5;
-
+        Debug.Log(init_Speed);
+        Debug.Log(init_Multiplier);
+        Debug.Log(init_SlowDeplete);
 
         UpdateUpgradeData();
     }
@@ -39,20 +38,45 @@ public class UpgradeManager : MonoBehaviour
     {
         for (var i = 0; i < init_Speed; i++)
         {
-            UpgradeArray1[i].color = new Color(1, 197, 0, 255);
+            UpgradeArray1[i].color = new Color32(1, 197, 0, 255);
         }
         for (var i = 0; i < init_Multiplier; i++)
         {
-            UpgradeArray2[i].color = new Color(100, 119, 198, 255);
+            UpgradeArray2[i].color = new Color32(100, 119, 198, 255);
         }
         for (var i = 0; i < init_SlowDeplete; i++)
         {
-            UpgradeArray3[i].color = new Color(198, 4, 0, 255);
+            UpgradeArray3[i].color = new Color32(198, 4, 0, 255);
         }
     }
 
-    private void SetUpgradeValue()
+    public void SetUpgradeValue(int index)
     {
-        gm.GetUpgradeDictionary()[ECollectible.SpeedCollectible] += 1;
+        switch (index)
+        {
+            case 0:
+                
+                gm.GetUpgradeDictionary()[ECollectible.SpeedCollectible] += 1;
+                break;
+            case 1:
+                gm.GetUpgradeDictionary()[ECollectible.MultiplierCollectible] += 1;
+                break;
+            case 2:
+                gm.GetUpgradeDictionary()[ECollectible.SlowDepleteCollectible] += 1;
+                break;
+            default:
+                break;
+        }
+                UpdateGameManagerData();
+
+    }
+
+    private void UpdateGameManagerData()
+    {
+        init_Speed = gm.GetUpgradeDictionary()[ECollectible.SpeedCollectible];
+        init_Multiplier = gm.GetUpgradeDictionary()[ECollectible.MultiplierCollectible];
+        init_SlowDeplete = gm.GetUpgradeDictionary()[ECollectible.SlowDepleteCollectible];
+
+        UpdateUpgradeData();
     }
 }
